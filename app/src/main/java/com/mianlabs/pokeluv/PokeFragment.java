6d9da872d03eaabc9cd8c42bcbd49035b2c22266
Damian Adams <reikjavic@live.com>
2016-09-02 21:12:25 -0400

    feat: Major, groundbreaking UI changes are made with a new retro typeface and some layout changes.

--- app/src/main/java/com/mianlabs/pokeluv/PokeFragment.java
+++ app/src/main/java/com/mianlabs/pokeluv/PokeFragment.java
@@ -3,6 +3,7 @@ package com.mianlabs.pokeluv;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -14,12 +15,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.github.florent37.picassopalette.PicassoPalette;
 import com.mianlabs.pokeluv.model.PokeModel;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
 import java.util.Random;
 
 import butterknife.BindView;
@@ -30,20 +33,24 @@ import me.sargunvohra.lib.pokekotlin.model.EvolutionChain;
 import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
 
+// TODO: Add documentation.
 public class PokeFragment extends Fragment {
     private static final String TAG = PokeFragment.class.getSimpleName();
     private Activity mContext;
+    private Typeface mCustomFont;
 
+    @BindView(R.id.pokemon_detail_container)
+    ScrollView mContainer;
     @BindView(R.id.pokemon_sprite)
     ImageView mPokemonSprite;
     @BindView(R.id.pokemon_number_border)
     TextView mPokemonNumBorder;
     @BindView(R.id.pokemon_text_name)
     TextView mPokemonName;
-    @BindView(R.id.pokemon_weight)
-    TextView mPokemonWeight;
     @BindView(R.id.pokemon_height)
     TextView mPokemonHeight;
+    @BindView(R.id.pokemon_weight)
+    TextView mPokemonWeight;
     @BindView(R.id.pokemon_types)
     TextView mPokemonTypes;
     @BindView(R.id.pokemon_color)
@@ -56,6 +63,10 @@ public class PokeFragment extends Fragment {
     TextView mPokemonGeneration;
     @BindView(R.id.pokemon_description)
     TextView mPokemonDescription;
+    @BindView(R.id.pokemon_evolutions)
+    TextView mPokemonEvolutions;
+    @BindView(R.id.pokemon_evo_line)
+    TextView mPokemonEvoLine;
 
     @Override
     public void onAttach(Context context) {
@@ -75,6 +86,9 @@ public class PokeFragment extends Fragment {
         View viewRoot = inflater.inflate(R.layout.fragment_poke, container, false);
         ButterKnife.bind(this, viewRoot);
         mContext = getActivity(); // Grabs the context from the parent activity.
+        mCustomFont = Typeface.createFromAsset(mContext.getAssets(), "fonts/Pokemon GB.ttf");
+        setCustomTypefaceForViews();
+        mContainer.setVisibility(View.INVISIBLE); // Hides the Views until properly set with Pokemon data.
 
         if (isNetworkAvailable()) {
             new Thread(new Runnable() { // Background thread for networking requests.
@@ -93,19 +107,36 @@ public class PokeFragment extends Fragment {
                     final PokeModel pokeModel = new PokeModel(pokemon, pokemonSpecies, evolutionChain);
                     Log.d(TAG, pokeModel.toString());
 
-                    Handler handler = new Handler(Looper.getMainLooper()); // grabs UI thread.
+                    Handler handler = new Handler(Looper.getMainLooper()); // Grabs UI thread.
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
                             loadSpriteAndPalettes(pokeModel);
                             setPokemonData(pokeModel);
                             Log.d(TAG, "Pokemon data set");
+                            mContainer.setVisibility(View.VISIBLE); // Views are ready to be displayed.
                         }
                     });
                 }
             }).start();
         }
+
         return viewRoot;
+    }
+
+    private void setCustomTypefaceForViews() {
+        mPokemonNumBorder.setTypeface(mCustomFont);
+        mPokemonName.setTypeface(mCustomFont);
+        mPokemonHeight.setTypeface(mCustomFont);
+        mPokemonWeight.setTypeface(mCustomFont);
+        mPokemonTypes.setTypeface(mCustomFont);
+        mPokemonColor.setTypeface(mCustomFont);
+        mPokemonShape.setTypeface(mCustomFont);
+        mPokemonHabitat.setTypeface(mCustomFont);
+        mPokemonGeneration.setTypeface(mCustomFont);
+        mPokemonDescription.setTypeface(mCustomFont);
+        mPokemonEvolutions.setTypeface(mCustomFont);
+        mPokemonEvoLine.setTypeface(mCustomFont);
     }
 
     /**
@@ -130,26 +161,29 @@ public class PokeFragment extends Fragment {
         mPokemonName.setText(pokeModel.getName());
         mPokemonWeight.setText("Weight: " + pokeModel.getWeight());
         mPokemonHeight.setText("Height: " + pokeModel.getHeight());
-
-        // Get the Pokemon type or types from the List.
-        String typing = pokeModel.getTypes().toString();
-        String types = "";
-        for (int i = 0; i < typing.length(); i++) {
-            char ch = typing.charAt(i);
-            if (ch != '[' && ch != ']') {
-                if (ch == ',')
-                    types += "\t\t"; // Put space between multiple types.
-                else
-                    types += Character.toUpperCase(ch);
-            }
-        }
-        mPokemonTypes.setText(types);
+        mPokemonTypes.setText(formatListToString(pokeModel.getTypes()));
         mPokemonColor.setText("Color: " + pokeModel.getColor());
         mPokemonShape.setText("Shape: " + pokeModel.getShape());
         if (pokeModel.getHabitat() != null)
             mPokemonHabitat.setText("Habitat: " + pokeModel.getHabitat());
         mPokemonGeneration.setText(pokeModel.getGeneration().toUpperCase());
         mPokemonDescription.setText(pokeModel.getDescription());
+        mPokemonEvoLine.setText(formatListToString(pokeModel.getEvolutions()));
+    }
+
+    private String formatListToString(List list) {
+        String listStr = list.toString();
+        String result = "";
+        for (int i = 0; i < listStr.length(); i++) {
+            char ch = listStr.charAt(i);
+            if (ch != '[' && ch != ']') {
+                if (ch == ',')
+                    result += "\t\t"; // Put space between multiple types.
+                else
+                    result += Character.toUpperCase(ch);
+            }
+        }
+        return result;
     }
 
     private void loadSpriteAndPalettes(PokeModel pokeModel) {
@@ -160,11 +194,13 @@ public class PokeFragment extends Fragment {
                         .intoTextColor(mPokemonNumBorder, PicassoPalette.Swatch.BODY_TEXT_COLOR) // Text color for Number.
                         .intoTextColor(mPokemonTypes, PicassoPalette.Swatch.BODY_TEXT_COLOR) // Text color for Types.
                         .intoTextColor(mPokemonGeneration, PicassoPalette.Swatch.BODY_TEXT_COLOR) // Text color for Generation.
+                        .intoTextColor(mPokemonEvolutions, PicassoPalette.Swatch.BODY_TEXT_COLOR) // Text color for Evolutions.
                         .use(PicassoPalette.Profile.VIBRANT_LIGHT)
                         .intoBackground(mPokemonNumBorder) // Background color for Number.
                         .intoBackground(mPokemonGeneration) // Background color for Generation.
                         .use(PicassoPalette.Profile.MUTED_LIGHT)
                         .intoBackground(mPokemonTypes) // Background color for Types.
+                        .intoBackground(mPokemonEvolutions) // Background color for Evolutions.
                 );
     }
 
